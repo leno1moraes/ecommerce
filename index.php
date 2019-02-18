a90f7c2191b7b1baa207c195b778cd14904c6ce3
Leno GraziannyMoraes <lenomoraes12@gmail.com>
2019-02-17 21:55:40 -0400

    Fazendo a Tpl de Admin

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\DB\Sql;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
-
 $app->config('debug', true);
 
 $app->get('/', function() {
@@ -18,6 +18,13 @@ $app->get('/', function() {
 	echo json_encode($results);*/
 
 	$page = new Page();
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
 	$page->setTpl("index");
 
 });
