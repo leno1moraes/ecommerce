454cc50f328085327ce7a5dfc84d8db24b33b1ce
Leno GraziannyMoraes <lenomoraes12@gmail.com>
2019-03-10 15:04:32 -0400

    Admin - Search usuario

--- admin-users.php
+++ admin-users.php
@@ -2,47 +2,61 @@
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 
-$app->get('/admin/users', function() {
-	User::verifyLogin();
-	$users = User::listAll();
-	$page = new PageAdmin();
-	$page->setTpl("users", array(
-		"users"=>$users
-	));
-});
-
 $app->get('/admin/users/create', function() {
+
 	User::verifyLogin();
+
 	$page = new PageAdmin();
+
 	$page->setTpl("users-create");
+
 });
 
 $app->get('/admin/users/:iduser/delete', function($iduser) {
+
 	User::verifyLogin();
+
 	$user = new User();
+
 	$user->get((int)$iduser);
+
 	$user->delete();
+
 	header("Location: /admin/users");
+
 	exit;	
 });
 
 $app->get('/admin/users/:iduser', function($iduser) {
+
 	User::verifyLogin();
+
 	$user = new User();
+
 	$user->get((int)$iduser);
+
 	$page = new PageAdmin();
+
 	$page->setTpl("users-update", array(
 		"user"=>$user->getValues()
 	));
+
 });
 
 $app->post('/admin/users/create', function() {
+
 	User::verifyLogin();
+
 	$users = new User();
+
 	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
- 	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, ["cost"=>12]);	
+
+ 	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, ["cost"=>12]);
+
 	$users->setData($_POST);
+
 	$users->save();
+
 	header("Location: /admin/users");
 	exit;
 });
@@ -56,6 +70,45 @@ $app->post('/admin/users/:iduser', function($iduser) {
 	$user->update();	
 	header("Location: /admin/users");
 	exit;	
+});
+
+
+$app->get('/admin/users', function() {
+	User::verifyLogin();
+
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+	if ($search != ''){
+														 /*Muda a quantidade de página aqui*/
+		$pagination = User::getPageSearch($search, $page, 1);
+
+	}else{
+										  /*Muda a quantidade de página aqui*/
+		$pagination = User::getPage($page, 1);
+
+	}
+
+	$pages = [];
+
+	for ($x=0; $x < $pagination['pages']; $x++) { 
+		array_push($pages, [
+			'href'=>'/admin/users?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+	}
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
+	));
+
 });
 
 ?>
