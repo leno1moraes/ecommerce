37c54f05da0cc8b1f83acad2d38e903a81578b3f
Leno GraziannyMoraes <lenomoraes12@gmail.com>
2019-03-10 15:31:47 -0400

    Admin - Search Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -28,12 +28,15 @@ class Category extends Model {
 	}
 
 	public function get($idcategory){
+		//if (!isset($idcategory)){
+
 		$sql = new Sql();
 
 		$results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory", [
 			':idcategory'=>$idcategory]);		
 
 		$this->setData($results[0]);
+		//}
 	}
 
 	public function delete(){
@@ -123,7 +126,51 @@ class Category extends Model {
 		]);
 	}
 
-	
+	public static function getPage($page = 1, $itemsPerPage = 10){		
+		
+		$start = ($page - 1 ) * $itemsPerPage;		
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT SQL_CALC_FOUND_ROWS * 
+								 FROM tb_categories 
+								 ORDER BY descategory
+								 LIMIT $start, $itemsPerPage;");
+
+		$resultTotal  = $sql->select("SELECT FOUND_ROWS() as nrtotal;");
+
+		return [
+			'data'=>$results, 
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+
+	}	
+
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 10){		
+		
+		$start = ($page - 1 ) * $itemsPerPage;		
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT SQL_CALC_FOUND_ROWS * 
+								 FROM tb_categories
+								 WHERE descategory LIKE :search
+								 ORDER BY descategory 
+								 LIMIT $start, $itemsPerPage", [
+								 	':search'=>'%'.$search.'%'
+								 ]);
+
+		$resultTotal  = $sql->select("SELECT FOUND_ROWS() as nrtotal;");
+
+		return [
+			'data'=>$results, 
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+		
+	}
+
 
 }
 
